77f6df32bd5f6f2004abb390e7b56a16b04b5378
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-03-26 09:57:20 -0300

    Stylecomponents

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+
+export const Container= styled.View`
+        flex: 1;
+        justify-Content: center;
+        align-Items: center;
+        background-color: #999;
+`;
