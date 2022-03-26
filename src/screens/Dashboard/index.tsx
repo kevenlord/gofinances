b3f410a6e11f136484557ee199accdbbb0752463
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-03-26 17:01:01 -0300

    FlatList
    E outras paradas...

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,103 @@
 import React from "react";
+import { HighlightCard } from "../../components/HighlighCard";
+import { TransactionCard } from "../../components/TransactionCard";
 
-import { Container, Title } from './styles'
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList,
+} from './styles';
 
 
 export function Dashboard() {
+    const data = [
+        {
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2022",
+        },
+        {
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2022",
+        },
+        {
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2022",
+        },
+
+    ]
+
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/39449222?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Keven</UserName>
+                        </User>
+                    </UserInfo>
+
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+
+            <HighlightCards>
+                <HighlightCard
+                    type="up"
+                    title="Entrada"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Útima entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Útima saída dia 03 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransaction="Útima entrada dia 13 de abril"
+                />
+            </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionList
+                    data={data}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                    showsVerticalScrollIndicator={false}
+                />
+
+            </Transactions>
+
         </Container>
     )
 }
