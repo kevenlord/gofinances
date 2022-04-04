1d71297869093f2939777c1e7ba1f058ba17589b
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-04-04 16:44:26 -0300

    Category
    Modal com as categorias criada com sucesso

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { FlatList } from "react-native";
+
+import { Button } from "../../components/Form/Button";
+
+import { categories } from "../../utils/categories";
+
 import { 
     Container,
     Headers,
     Title,
-
+    Category,
+    Icon,
+    Name,
+    Separator,
+    Footer,
 } from "./styles";
 
 interface Category{
@@ -12,17 +22,45 @@ interface Category{
 }
 
 interface Props{
-    category: string;
+    category: Category;
     setCategory: (category: Category) => void;
     closeSelectCategory: () => void;
 }
  
 export function CategorySelect({category, setCategory, closeSelectCategory}: Props){
+    function handleCategorySelect(category: Category){
+        setCategory(category);
+    }
+
+
     return(
         <Container>
             <Headers>
                 <Title>Categoria</Title>
             </Headers>
+
+            <FlatList 
+                data={categories}
+                style={{ flex: 1, width: '100%'}}
+                keyExtractor={(item) => item.key}
+                renderItem={({item}) => (
+                <Category
+                    onPress={() => handleCategorySelect(item)}
+                    isActive={category.key === item.key}
+                >
+                    <Icon name={item.icon} />
+                    <Name>{item.name}</Name>
+                </Category>
+                
+                )}
+                ItemSeparatorComponent={()=> <Separator/>}
+            />
+
+            <Footer>
+                <Button title="Selecionar"
+                    onPress={closeSelectCategory}
+                />
+            </Footer>
         </Container>
     )
 }
